5c7ea294bc6d2c44e1539a4889703cb733ab11f1
p.bertran <p.bertran@qwant.com>
2018-10-05 16:29:43 +0200

    "v2.3.0 - removing_siri2"

--- Client/Frontend/Settings/AppSettingsOptions.swift
+++ Client/Frontend/Settings/AppSettingsOptions.swift
@@ -706,6 +706,10 @@ class SendFeedbackSetting: Setting {
     override func onClick(_ navigationController: UINavigationController?) {
         setUpAndPushSettingsContentViewController(navigationController)
     }
+    
+    override var hidden: Bool {
+        return true
+    }
 }
 
 class SendAnonymousUsageDataSetting: BoolSetting {
@@ -1058,6 +1062,10 @@ class SiriPageSetting: Setting {
         let viewController = SiriSettingsViewController(prefs: profile.prefs)
         viewController.profile = profile
         navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    override var hidden: Bool {
+        return true
     }
 }
 
