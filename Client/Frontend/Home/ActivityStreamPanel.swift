ebadf9d25eaf453db925730fec80599e9f9f25d4
p.bertran <p.bertran@qwant.com>
2019-01-21 17:25:53 +0100

    fixed ActivityStreamPanel

--- Client/Frontend/Home/ActivityStreamPanel.swift
+++ Client/Frontend/Home/ActivityStreamPanel.swift
@@ -491,7 +491,7 @@ extension ActivityStreamPanel: DataObserverDelegate {
 
             // How sites are merged together. We compare against the url's base domain. example m.youtube.com is compared against `youtube.com`
             let unionOnURL = { (site: Site) -> String in
-                if URL(string: site.url)?.hostSLD == "qwant" {
+                if URL(string: site.url)?.shortDisplayString == "qwant" {
                     return URL(string: site.url)?.absoluteString ?? ""
                 }
                 return URL(string: site.url)?.normalizedHost ?? ""
@@ -513,7 +513,7 @@ extension ActivityStreamPanel: DataObserverDelegate {
                     return site
                 }
 
-                var domain = URL(string: site.url)?.hostSLD
+                var domain = URL(string: site.url)?.shortDisplayString
                 if (domain == "qwant") {
                     domain = URL(string: site.url)?.host
                 } else {
