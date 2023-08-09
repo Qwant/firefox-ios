7d54a7e3935f793483b1c7920e7bc8c55538be19
Jérôme Boursier <j.boursier@qwant.com>
2023-08-09 09:30:10 +0200

    [Qwant] [MOBILE-250] - Toggling theme breaks suggest

--- Client/Frontend/Widgets/AutocompleteTextField.swift
+++ Client/Frontend/Widgets/AutocompleteTextField.swift
@@ -259,6 +259,14 @@ class AutocompleteTextField: UITextField, UITextFieldDelegate {
     override func caretRect(for position: UITextPosition) -> CGRect {
         return hideCursor ? CGRect.zero : super.caretRect(for: position)
     }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        if let base = text, let autocomplete = autocompleteTextLabel?.text {
+            setAutocompleteSuggestion(base + autocomplete)
+        }
+    }
 
     private func createAutocompleteLabelWith(_ autocompleteText: NSAttributedString) -> UILabel {
         let label = UILabel()
